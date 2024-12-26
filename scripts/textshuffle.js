e999076d12531e5d36c0451f3a1aa50d385f6b62
Kevin Chang <changkevin51@gmail.com>
2024-12-25 20:54:24 -0500

    CHRISTMAS UPDATE

--- scripts/textshuffle.js
+++ scripts/textshuffle.js
@@ -1,23 +1,37 @@
 $(document).ready(function () {
-    // Fetch the words from words.json
+    // Shuffle Words
     $.getJSON('./words.json', function (data) {
-        const words = data.words; // Extract the array of words
-        let index = 0; // Current word index
+        const words = data.words;
+        let wordIndex = 0;
 
-        // Function to update the word
         function updateWord() {
             const wordElement = $('#words');
             wordElement.fadeOut(500, function () {
-                // Update the text after fading out
-                wordElement.text(words[index]);
+                wordElement.text(words[wordIndex]);
                 wordElement.fadeIn(500);
             });
 
-            // Move to the next word in the array
-            index = (index + 1) % words.length;
+            wordIndex = (wordIndex + 1) % words.length;
         }
 
-        // Start the cycling effect
-        setInterval(updateWord, 3000); // Change every 3 seconds
+        setInterval(updateWord, 3000);
+    });
+
+    // Shuffle Locations
+    $.getJSON('./locations.json', function (data) {
+        const locations = data.locations;
+        let locationIndex = 0;
+
+        function updateLocation() {
+            const locationElement = $('#location');
+            locationElement.fadeOut(500, function () {
+                locationElement.text(locations[locationIndex]);
+                locationElement.fadeIn(500);
+            });
+
+            locationIndex = (locationIndex + 1) % locations.length;
+        }
+
+        setInterval(updateLocation, 3000);
     });
 });
